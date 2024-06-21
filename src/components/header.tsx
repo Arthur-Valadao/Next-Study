a12db368063068c5956866603431b82e1b37ffbb
Arthur <Arth.valadao@gmail.com>
2024-06-20 23:55:13 -0300

    insert lucide icons and create header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,23 @@
+import { Search } from 'lucide-react'
+import Link from 'next/link'
+
 export function Header() {
-  return <h1>Headers</h1>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devStore
+        </Link>
+
+        <form
+          className="flex w-[320px] items-center gap-3 
+        rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+        >
+          <Search />
+        </form>
+      </div>
+
+      <div className="flex items-center gap-4"></div>
+    </div>
+  )
 }
